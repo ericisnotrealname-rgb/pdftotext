86e891a83ce327ed3c2d5f1fc205103f060e9f60
Jason Alan Palmer <jalanpalmer@gmail.com>
2022-12-24 16:45:27 -0700

    Add a poppler_version function

--- pdftotext.cpp
+++ pdftotext.cpp
@@ -245,10 +245,22 @@ static PyTypeObject PDFType = {
 
 static void do_nothing(const std::string&, void*) {}
 
+static PyObject* poppler_version(PyObject* self, PyObject* args) {
+    return PyUnicode_FromString(poppler::version_string().c_str());
+}
+
+static PyMethodDef Methods[] = {
+    {
+        "poppler_version",
+        poppler_version,
+        METH_NOARGS,
+        "Get the version of the poppler library in use.",
+    },
+    {NULL, NULL, 0, NULL}, // sentinel
+};
+
 static PyModuleDef pdftotextmodule = {
-    PyModuleDef_HEAD_INIT,
-    "pdftotext",
-    "Simple PDF text extraction.",
+    PyModuleDef_HEAD_INIT, "pdftotext", "Simple PDF text extraction.", -1, Methods,
 };
 
 PyMODINIT_FUNC PyInit_pdftotext() {
