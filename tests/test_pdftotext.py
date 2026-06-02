4ee072347018b4e97d4631d10e7386f6f0f844c9
Jason Alan Palmer <jalanpalmer@gmail.com>
2026-06-01 20:52:00 -0600

    Include type annotations

--- tests/test_pdftotext.py
+++ tests/test_pdftotext.py
@@ -11,7 +11,7 @@ for f in directory.iterdir():
         test_files[f.name] = io.BytesIO(f.read_bytes())
 
 
-def get_file(name):
+def get_file(name: str) -> io.BytesIO:
     """Return a copy of the requested test file as if it were just opened."""
     return io.BytesIO(test_files[name].getvalue())
 
@@ -19,55 +19,55 @@ def get_file(name):
 class InitTest(unittest.TestCase):
     """Test using and abusing __init__."""
 
-    def test_double_init_success(self):
+    def test_double_init_success(self) -> None:
         pdf = pdftotext.PDF(get_file("abcde.pdf"))
         pdf.__init__(get_file("blank.pdf"))
         self.assertEqual(len(pdf), 1)
 
-    def test_double_init_failure(self):
+    def test_double_init_failure(self) -> None:
         pdf = pdftotext.PDF(get_file("blank.pdf"))
         with self.assertRaises(AttributeError):
             pdf.__init__("wrong")
 
-    def test_init_file_in_text_mode(self):
+    def test_init_file_in_text_mode(self) -> None:
         text_file = io.StringIO("wrong")
         with self.assertRaises((pdftotext.Error, TypeError)):
             pdftotext.PDF(text_file)
 
-    def test_init_invalid_pdf_file(self):
+    def test_init_invalid_pdf_file(self) -> None:
         pdf_file = io.BytesIO(b"wrong")
         with self.assertRaises(pdftotext.Error):
             pdftotext.PDF(pdf_file)
 
-    def test_init_corrupt_pdf_file(self):
+    def test_init_corrupt_pdf_file(self) -> None:
         with self.assertRaises(pdftotext.Error):
             pdftotext.PDF(get_file("corrupt.pdf"))
 
-    def test_no_init(self):
+    def test_no_init(self) -> None:
         class BrokenPDF(pdftotext.PDF):
-            def __init__(self):
+            def __init__(self) -> None:
                 pass
 
         pdf = BrokenPDF()
         self.assertEqual(len(pdf), 0)
 
-    def test_locked_with_only_user_password(self):
+    def test_locked_with_only_user_password(self) -> None:
         with self.assertRaises(pdftotext.Error):
             pdftotext.PDF(get_file("user_password.pdf"))
 
-    def test_locked_with_only_user_password_user_unlock(self):
+    def test_locked_with_only_user_password_user_unlock(self) -> None:
         pdf = pdftotext.PDF(get_file("user_password.pdf"), "user_password")
         self.assertIn("secret", pdf[0])
 
-    def test_locked_with_both_passwords(self):
+    def test_locked_with_both_passwords(self) -> None:
         with self.assertRaises(pdftotext.Error):
             pdftotext.PDF(get_file("both_passwords.pdf"))
 
-    def test_locked_with_both_passwords_user_unlock(self):
+    def test_locked_with_both_passwords_user_unlock(self) -> None:
         pdf = pdftotext.PDF(get_file("both_passwords.pdf"), "user_password")
         self.assertIn("secret", pdf[0])
 
-    def test_locked_with_both_passwords_owner_unlock(self):
+    def test_locked_with_both_passwords_owner_unlock(self) -> None:
         pdf = pdftotext.PDF(get_file("both_passwords.pdf"), "owner_password")
         self.assertIn("secret", pdf[0])
 
@@ -75,12 +75,12 @@ class InitTest(unittest.TestCase):
 class GetItemTest(unittest.TestCase):
     """Test the __getitem__ method."""
 
-    def test_read(self):
+    def test_read(self) -> None:
         pdf = pdftotext.PDF(get_file("abcde.pdf"))
         result = pdf[0]
         self.assertIn("abcde", result)
 
-    def test_read_portrait(self):
+    def test_read_portrait(self) -> None:
         pdf = pdftotext.PDF(get_file("portrait.pdf"))
         result = pdf[0]
         self.assertIn("a", result)
@@ -88,7 +88,7 @@ class GetItemTest(unittest.TestCase):
         self.assertIn("c", result)
         self.assertIn("d", result)
 
-    def test_read_landscape_0(self):
+    def test_read_landscape_0(self) -> None:
         pdf = pdftotext.PDF(get_file("landscape_0.pdf"))
         result = pdf[0]
         self.assertIn("a", result)
@@ -96,7 +96,7 @@ class GetItemTest(unittest.TestCase):
         self.assertIn("c", result)
         self.assertIn("d", result)
 
-    def test_read_landscape_90(self):
+    def test_read_landscape_90(self) -> None:
         pdf = pdftotext.PDF(get_file("landscape_90.pdf"))
         result = pdf[0]
         self.assertIn("a", result)
@@ -104,7 +104,7 @@ class GetItemTest(unittest.TestCase):
         self.assertIn("c", result)
         self.assertIn("d", result)
 
-    def test_read_columns(self):
+    def test_read_columns(self) -> None:
         pdf = pdftotext.PDF(get_file("three_columns.pdf"))
         page = pdf[0]
         col1_index = page.index("column 1")
@@ -119,31 +119,31 @@ class GetItemTest(unittest.TestCase):
         self.assertLess(two_index, col3_index)
         self.assertLess(col3_index, three_index)
 
-    def test_no_doc_to_read(self):
+    def test_no_doc_to_read(self) -> None:
         class BrokenPDF(pdftotext.PDF):
-            def __init__(self):
+            def __init__(self) -> None:
                 pass
 
         pdf = BrokenPDF()
         with self.assertRaises(IndexError):
             pdf[0]
 
-    def test_pdf_read_invalid_page_number(self):
+    def test_pdf_read_invalid_page_number(self) -> None:
         pdf = pdftotext.PDF(get_file("blank.pdf"))
         with self.assertRaises(IndexError):
             pdf[100]
 
-    def test_pdf_read_wrong_arg_type(self):
+    def test_pdf_read_wrong_arg_type(self) -> None:
         pdf = pdftotext.PDF(get_file("blank.pdf"))
         with self.assertRaises(TypeError):
             pdf["wrong"]
 
-    def test_read_corrupt_page(self):
+    def test_read_corrupt_page(self) -> None:
         with self.assertRaises((pdftotext.Error, IndexError)):
             pdf = pdftotext.PDF(get_file("corrupt_page.pdf"))
             pdf[0]
 
-    def test_read_page_two(self):
+    def test_read_page_two(self) -> None:
         pdf = pdftotext.PDF(get_file("two_pages.pdf"))
         result = pdf[1]
         self.assertIn("two", result)
@@ -152,17 +152,17 @@ class GetItemTest(unittest.TestCase):
 class LengthTest(unittest.TestCase):
     """Test the __len__ method."""
 
-    def test_length_one(self):
+    def test_length_one(self) -> None:
         pdf = pdftotext.PDF(get_file("blank.pdf"))
         self.assertEqual(len(pdf), 1)
 
-    def test_length_two(self):
+    def test_length_two(self) -> None:
         pdf = pdftotext.PDF(get_file("two_pages.pdf"))
         self.assertEqual(len(pdf), 2)
 
-    def test_length_no_doc(self):
+    def test_length_no_doc(self) -> None:
         class BrokenPDF(pdftotext.PDF):
-            def __init__(self):
+            def __init__(self) -> None:
                 pass
 
         pdf = BrokenPDF()
@@ -172,24 +172,24 @@ class LengthTest(unittest.TestCase):
 class ListTest(unittest.TestCase):
     """Test iterating over pages."""
 
-    def test_list_first_element(self):
+    def test_list_first_element(self) -> None:
         pdf = pdftotext.PDF(get_file("two_pages.pdf"))
         self.assertIn("one", pdf[0])
 
-    def test_list_second_element(self):
+    def test_list_second_element(self) -> None:
         pdf = pdftotext.PDF(get_file("two_pages.pdf"))
         self.assertIn("two", pdf[1])
 
-    def test_list_invalid_element(self):
+    def test_list_invalid_element(self) -> None:
         pdf = pdftotext.PDF(get_file("two_pages.pdf"))
         with self.assertRaises(IndexError):
             pdf[2]
 
-    def test_list_last_element(self):
+    def test_list_last_element(self) -> None:
         pdf = pdftotext.PDF(get_file("two_pages.pdf"))
         self.assertIn("two", pdf[-1])
 
-    def test_for_loop(self):
+    def test_for_loop(self) -> None:
         pdf = pdftotext.PDF(get_file("two_pages.pdf"))
         result = ""
         for page in pdf:
@@ -201,21 +201,21 @@ class ListTest(unittest.TestCase):
 class RawTest(unittest.TestCase):
     """Test reading in raw layout."""
 
-    def test_raw_vs_not(self):
+    def test_raw_vs_not(self) -> None:
         filename = "table.pdf"
         pdf = pdftotext.PDF(get_file(filename))
         raw_pdf = pdftotext.PDF(get_file(filename), raw=True)
         self.assertNotEqual(pdf[0], raw_pdf[0])
 
-    def test_raw_invalid_type(self):
+    def test_raw_invalid_type(self) -> None:
         with self.assertRaises(TypeError):
             pdftotext.PDF(get_file("blank.pdf"), raw="")
 
-    def test_raw_invalid_value(self):
+    def test_raw_invalid_value(self) -> None:
         with self.assertRaises(ValueError):
             pdftotext.PDF(get_file("blank.pdf"), raw=100)
 
-    def test_raw_is_not_default(self):
+    def test_raw_is_not_default(self) -> None:
         filename = "table.pdf"
         pdf_default = pdftotext.PDF(get_file(filename))
         pdf_raw_false = pdftotext.PDF(get_file(filename), raw=False)
@@ -225,31 +225,31 @@ class RawTest(unittest.TestCase):
 class PhysicalTest(unittest.TestCase):
     """Test reading in physical layout."""
 
-    def test_physical_vs_not(self):
+    def test_physical_vs_not(self) -> None:
         filename = "three_columns.pdf"
         pdf = pdftotext.PDF(get_file(filename))
         physical_pdf = pdftotext.PDF(get_file(filename), physical=True)
         self.assertNotEqual(pdf[0], physical_pdf[0])
 
-    def test_physical_invalid_type(self):
+    def test_physical_invalid_type(self) -> None:
         with self.assertRaises(TypeError):
             pdftotext.PDF(get_file("blank.pdf"), physical="")
 
-    def test_physical_invalid_value(self):
+    def test_physical_invalid_value(self) -> None:
         with self.assertRaises(ValueError):
             pdftotext.PDF(get_file("blank.pdf"), physical=-10)
 
-    def test_physical_is_not_default(self):
+    def test_physical_is_not_default(self) -> None:
         filename = "three_columns.pdf"
         pdf_default = pdftotext.PDF(get_file(filename))
         pdf_physical_false = pdftotext.PDF(get_file(filename), physical=False)
         self.assertEqual(pdf_default[0], pdf_physical_false[0])
 
-    def test_raw_and_physical(self):
+    def test_raw_and_physical(self) -> None:
         with self.assertRaises(ValueError):
             pdftotext.PDF(get_file("blank.pdf"), raw=True, physical=True)
 
-    def test_raw_vs_physical(self):
+    def test_raw_vs_physical(self) -> None:
         filename = "three_columns.pdf"
         pdf_raw = pdftotext.PDF(get_file(filename), raw=True)
         pdf_physical = pdftotext.PDF(get_file(filename), physical=True)
